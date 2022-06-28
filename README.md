28952153bca3b4cab18fea94e7e71c6d92537b56
vlphx <alphonsevivien@gmail.com>
2022-06-28 13:38:01 +0200

    adding project

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# New Project
+# Instructions pour l'installation 
+    Veuillez exécuter la commande: npm install
+# Les scripts
+    commande: npm run start
+    (Exécute l'application en mode développement.
+    Ouvrez http://localhost:8080 pour l'afficher dans le navigateur.)
 
-> ✨ Bootstrapped with Create Snowpack App (CSA).
-
-## Available Scripts
-
-### npm start
-
-Runs the app in the development mode.
-Open http://localhost:8080 to view it in the browser.
-
-The page will reload if you make edits.
-You will also see any lint errors in the console.
-
-### npm run build
-
-Builds a static copy of your site to the `build/` folder.
-Your app is ready to be deployed!
-
-**For the best production performance:** Add a build bundler plugin like [@snowpack/plugin-webpack](https://github.com/snowpackjs/snowpack/tree/main/plugins/plugin-webpack) or [snowpack-plugin-rollup-bundle](https://github.com/ParamagicDev/snowpack-plugin-rollup-bundle) to your `snowpack.config.mjs` config file.
-
-### Q: What about Eject?
-
-No eject needed! Snowpack guarantees zero lock-in, and CSA strives for the same.
+    commande: npm run build
+    (Construit une copie statique du site dans le dossier `build/`.)
+# Comment ça marche ?
+    Saisissez les informations demandées dans le formulaire.
+    Ensuite appuyez sur le bouton 'valider'
+    Le résultat s'affichera en dessous du formulaire.
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,141 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+type vacuum = {
+  x: number,
+  y: number,
+  orientation:string
+}
+type grid = {
+  x: number,
+  y: number,
+}
+type gridCell = {
+  N: boolean,
+  E: boolean,
+  S: boolean,
+  O: boolean,
+}
+const directionCombinaisons: Object = { 'N': { 'G': 'O', 'D': 'E' }, 'E': { 'G': 'N', 'D': 'S' }, 'S': { 'G': 'E', 'D': 'O' }, 'O': { 'G': 'S', 'D': 'N' } } 
 
-import confetti from 'canvas-confetti';
+const message = document.querySelector<HTMLHeadingElement>('#answer');
+const form = document.querySelector<HTMLFormElement>('#form');
+const dimensionX = document.querySelector<HTMLInputElement>('#dimensionX');
+const dimensionY = document.querySelector<HTMLInputElement>('#dimensionY');
+const initialVacuumPositionX = document.querySelector<HTMLInputElement>('#initialVacuumPositionX');
+const initialVacuumPositionY = document.querySelector<HTMLInputElement>('#initialVacuumPositionY');
+const addInstructionG = document.querySelector<HTMLFormElement>('#addInstructionG');
+const addInstructionD = document.querySelector<HTMLFormElement>('#addInstructionD');
+const addInstructionA = document.querySelector<HTMLFormElement>('#addInstructionA');
+const showInstructions = document.querySelector<HTMLParagraphElement>('#showInstructions');
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+let instructions: Array<string> = [];
+let finalInstructions: Array<string> = [];
+let grille: grid;
+let ihoover: vacuum;
+
+
+addInstructionG?.addEventListener('click', (e) => {
+  instructions.push('G')
+  showInstructions?.append('G')
+})
+addInstructionD?.addEventListener('click', (e) => {
+  instructions.push('D')
+  showInstructions?.append('D')
+})
+addInstructionA?.addEventListener('click', (e) => {
+  instructions.push('A')
+  showInstructions?.append('A')
+})
+
+form?.addEventListener('submit', (e) => {
+  const initialVacuumOrientation = document.querySelector<HTMLInputElement>('input[name="orientation"]:checked')?.value;
+  e.preventDefault();
+  
+
+  if (dimensionX?.value == null || dimensionX?.value == '' || dimensionY?.value == null || dimensionY?.value == '' || initialVacuumPositionX?.value == null || initialVacuumPositionX?.value == '' || initialVacuumPositionY?.value == null || initialVacuumPositionY?.value == '' || initialVacuumOrientation == null || initialVacuumOrientation == '' || parseInt(initialVacuumPositionX?.value) > parseInt(dimensionX?.value) || parseInt(initialVacuumPositionY?.value) > parseInt(dimensionY?.value) || instructions.length == 0) {
+    if (message == null) return
+    message.innerHTML = 'Erreur';
+    return
+  }
+
+   grille = {x : parseInt(dimensionX.value), y : parseInt(dimensionY.value)} 
+   ihoover = { x : parseInt(initialVacuumPositionX.value) , y: parseInt(initialVacuumPositionY.value) , orientation: initialVacuumOrientation};
+   finalInstructions = instructions
+
+  instructions = []
+  createGrille(grille, ihoover, finalInstructions)
+});
+
+const addInstruction = (instruction: string) => {
+  instructions.push(instruction)
+}
+
+const createGrille = (grille: grid, aspirateur: vacuum, finalInstructions: Array<string>) => {
+  let gridObject: Object = {}
+  for (let i = 0; i < grille.x; i++) {
+    for (let j = 0; j < grille.y; j++)
+    {
+      let caseGrille: gridCell = { N: true, E: true, S: true, O:true };
+      if (i == 0 && j == 0) { 
+        caseGrille.S = false
+        caseGrille.O = false
+      }
+      if (i == grille.x - 1 && j == 0) {
+        caseGrille.S = false
+        caseGrille.E = false
+      }
+      if (i == 0 && j == grille.y - 1) {
+        caseGrille.N = false
+        caseGrille.O = false
+      }
+      if (i == grille.x - 1 && j == grille.y - 1) {
+        caseGrille.N = false
+        caseGrille.E = false
+      }
+      let tempObj = {}
+      tempObj[j] = caseGrille
+      let oldData = gridObject[i]
+      gridObject[i] = { ...oldData, ...tempObj }
+      }
+  }
+  ihooverGo(gridObject, aspirateur, finalInstructions)
+}
+
+const ihooverGo = (currentGrid: Object, ihoover: vacuum, finalInstructions: Array<string>) => {
+  for (let i = 0; i < finalInstructions.length; i++) {
+    if (finalInstructions[i] == 'A') { 
+      if (currentGrid[ihoover.x -1][ihoover.y -1][ihoover.orientation]) {
+        ihoover = moveForward(ihoover)
+
+      }
+    }
+    else {
+      ihoover.orientation = directionCombinaisons[ihoover.orientation][finalInstructions[i]]
+    }
+  }
+  showAnswer(ihoover)
+}
+
+const moveForward = (aspirateur: vacuum) => {
+  if (aspirateur.orientation == 'S') {
+    aspirateur.y -= 1
+  }
+  else if (aspirateur.orientation == 'N') {
+    aspirateur.y += 1
+  }
+  else if(aspirateur.orientation == 'O') {
+    aspirateur.x -= 1
+  }
+  else {
+    aspirateur.x += 1
+  }
+  return aspirateur
+
+}
+
+const showAnswer = (aspirateur: vacuum) => {
+  let answer = "La position finale : x=" + aspirateur.x + " y=" + aspirateur.y + " orientation=" + aspirateur.orientation;
+  if (message == null) {
+    console.log(aspirateur);
+    return
+  }
+  message.innerHTML = (answer)
+}
